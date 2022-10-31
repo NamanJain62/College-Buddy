f8673e6869592745464ec566a55df86874419007
NamanJain62 <20CBS1062@cuchd.in>
2022-10-31 19:04:26 +0530

    version 1.1.0

---
+++ app/src/main/java/com/naman/collegebuddy/login_activity.kt
@@ -0,0 +1,11 @@
+package com.naman.collegebuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
