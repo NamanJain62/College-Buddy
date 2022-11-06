b7d5ddb22f78daad119d71fb21a5d73f5b0dcb3b
NamanJain62 <20CBS1062@cuchd.in>
2022-11-06 22:37:42 +0530

    Register yourself page working

---
+++ app/src/main/java/com/naman/collegebuddy/RegisterYourself.kt
@@ -0,0 +1,11 @@
+package com.naman.collegebuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterYourself : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registeryourself)
+    }
+}
